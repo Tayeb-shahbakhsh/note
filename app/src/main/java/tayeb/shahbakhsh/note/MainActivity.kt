dfbae7b4dd54179edd1ff0f2a74989c28216f5e2
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-03-28 18:14:44 +0430

    recycler view ready to use

--- app/src/main/java/tayeb/shahbakhsh/note/MainActivity.kt
+++ app/src/main/java/tayeb/shahbakhsh/note/MainActivity.kt
@@ -2,10 +2,20 @@ package tayeb.shahbakhsh.note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import tayeb.shahbakhsh.note.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.notesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = NotesAdapter(this@MainActivity)
+        }
     }
 }
