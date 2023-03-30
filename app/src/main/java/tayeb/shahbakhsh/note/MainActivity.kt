2b3b582fc1ec0bae067b39bfdf53b1ab60841768
Tayeb-shahbakhsh <shahbakhshtayyeb@gmail.com>
2023-03-30 16:21:43 +0430

    add note to adapter completed

--- app/src/main/java/tayeb/shahbakhsh/note/MainActivity.kt
+++ app/src/main/java/tayeb/shahbakhsh/note/MainActivity.kt
@@ -1,7 +1,9 @@
 package tayeb.shahbakhsh.note
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import androidx.dynamicanimation.animation.SpringAnimation
 import androidx.dynamicanimation.animation.SpringForce
@@ -9,18 +11,23 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import tayeb.shahbakhsh.note.databinding.ActivityMainBinding
+import tayeb.shahbakhsh.note.model.Note
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var notes = mutableListOf<Note>()
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.notesRecyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = NotesAdapter(this@MainActivity)
+            adapter = NotesAdapter(this@MainActivity,notes)
         }
 
         val bottomSheet = binding.addNoteBottomSheet
@@ -43,6 +50,29 @@ class MainActivity : AppCompatActivity() {
 
             animateFab(binding.addNoteFab, bottomSheetIsHidden)
         }
+
+        binding.saveNoteButton.setOnClickListener {
+            saveNote()
+            binding.addNoteFab.callOnClick()
+            binding.notesRecyclerView.adapter?.notifyDataSetChanged()
+
+            //clear edit texts
+            binding.titleEditText.setText("")
+            binding.captionEditText.setText("")
+        }
+    }
+
+    private fun saveNote() {
+        var newNote: Note
+        binding.apply {
+            newNote = Note(
+                titleEditText.text.toString(),
+                captionEditText.text.toString(),
+                "1401.1.1"
+            )
+        }
+
+        notes.add(newNote)
     }
 
     private fun animateFab(fab: FloatingActionButton, bottomSheetIsHidden: Boolean) {
@@ -62,5 +92,4 @@ class MainActivity : AppCompatActivity() {
             springAnim.start()
         }
     }
-
 }
---
+++ app/src/main/java/tayeb/shahbakhsh/note/model/Note.kt
@@ -0,0 +1,3 @@
+package tayeb.shahbakhsh.note.model
+
+data class Note(var title: String, var caption: String, var date: String)
